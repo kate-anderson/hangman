9716a79f75a4bfd619d36d35131b186eea14ed05
Kate Anderson <kate-anderson@uiowa.edu>
2017-12-02 11:46:47 -0600

    created the compare function

--- src/hangman/Utilities.java
+++ src/hangman/Utilities.java
@@ -1,12 +1,28 @@
 package hangman;
 
+import edu.princeton.cs.introcs.*;
+
 public class Utilities {
 
 	public static String randomWord(String[] args) {
-		// read in the number of words in the file
+		// read in the number of words in the words file
+		int numWords = StdIn.readInt();
+		
 		// pick a random word out of the file
-		// return the word
-
+		int r = (int)(Math.random()*(numWords-1));
+		String word = Files.readAllLines(Paths.get("words.txt")).get(r);
+			// pick a random number 0 to numWords-1
+			// get that word from the file
+		return word;
+	}
+	
+	public static void compare(char guess) {
+		String word = randomWord();
+		//find the index of the letter (neg number if it is not in the word)
+		int position = word.indexOf(guess);
+		if(position<0) {
+			// draw part of the man
+		}
 	}
 
 }
