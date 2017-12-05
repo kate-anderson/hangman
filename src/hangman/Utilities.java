eb43c1185b0213fa5762161f6e6e131f54c577a9
u2huebui <31355459+u2huebui@users.noreply.github.com>
2017-12-05 06:27:54 -0600

    hangman game
    I did the hangman, but it has a problem. when the word have two letters duplicated, it just draw one letter on screen.
    In the main function, I did use scanner class. I actually not really understand scanner class, and think about using StdIn. I did try the StdIn, but I did wrong somewhere, so it didn't work. Can you help me fix those errors

--- src/hangman/Utilities.java
+++ src/hangman/Utilities.java
@@ -1,28 +1,99 @@
-package hangman;
+package hangman1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.awt.Font;
 
 import edu.princeton.cs.introcs.*;
 
 public class Utilities {
 
-	public static String randomWord(String[] args) {
+	public static String randomWord() {
 		// read in the number of words in the words file
-		int numWords = StdIn.readInt();
-		
-		// pick a random word out of the file
-		int r = (int)(Math.random()*(numWords-1));
-		String word = Files.readAllLines(Paths.get("words.txt")).get(r);
-			// pick a random number 0 to numWords-1
-			// get that word from the file
-		return word;
+		ArrayList<String> arr = new ArrayList<String>();//create an array to contain the words
+        try (BufferedReader br = new BufferedReader(new FileReader("words")))
+        {
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+                arr.add(sCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Random randomGenerator = new Random();
+      
+        int index = randomGenerator.nextInt(arr.size());//pick a random order of the word
+        String item = arr.get(index); //read a word from array of words
+     
+        return item;
+        
 	}
-	
-	public static void compare(char guess) {
-		String word = randomWord();
-		//find the index of the letter (neg number if it is not in the word)
+
+	public static int compare(char guess, String word) {
+//		String word = randomWord();
+		// find the index of the letter (neg number if it is not in the word)
 		int position = word.indexOf(guess);
-		if(position<0) {
-			// draw part of the man
+		return position;
+	}
+
+public static void initSketch() {
+		//draw a hang and canvas
+		StdDraw.setXscale(-0.5, +3.5);
+		StdDraw.setYscale(-0.5, +3.0);
+		StdDraw.setPenRadius(0.005);
+		
+		StdDraw.line(-0.3, -0.3, 0.2, -0.3);
+		StdDraw.line(-0.05, -0.3, -0.05, 1.45);
+		StdDraw.line(-0.05, 1.45, 0.65, 1.45);
+		StdDraw.line(0.65, 1.45, 0.65, 1.35);
+	}
+
+	public static void hangman(int n) {
+
+		if (n >= 1) {
+			StdDraw.circle(0.65, 1.2875, 0.0625); // draw a head
+		}
+		if (n >= 2) {
+			StdDraw.line(0.65, 1.225, 0.65, 1.1); // draw a kneck
+		}
+		if (n >= 3) {
+			StdDraw.line(0.65, 1.1, 0.30, 0.85); // draw a hang
+		}
+		if (n >= 4) {
+			StdDraw.line(0.65, 1.1, 1.0, 0.85); // draw a hang
+		}
+		if (n >= 5) {
+			StdDraw.line(0.65, 1.1, 0.65, 0.65); // draw a body
+		}
+		if (n >= 6) {
+			StdDraw.line(0.65, 0.65, 0.35, 0.125); // draw a leg
+		}
+		if (n >= 7) {
+			StdDraw.line(0.65, 0.65, 1.0, 0.125); // draw a leg
 		}
 	}
 
+public static void drawBlank(String word) {
+//draw the blank base on the length of word
+		
+		int length = word.length();//length of the letter
+		double x0 = -0.2;
+		for (int i = 0; i < length; i++) {
+			StdDraw.line(x0, 2.0, x0 + 0.2, 2.0);
+			x0+=0.3;
+		}
+}
+
+public static void drawLetter(int p, String c) {
+//	StdDraw.text(-0.15, 2.1, "V"); // write the letter on the screen
+	StdDraw.text(-0.1 + 0.3*p, 2.1, c); 
+//	Font stringFont = new Font( "SansSerif", Font.PLAIN, 44);
+//	StdDraw.setFont(SansSerif);
+}
 }
