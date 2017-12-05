fffb389ed48d406ed2a2074972fc4fd7d1b9c90a
Kate Anderson <kate-anderson@uiowa.edu>
2017-12-04 20:15:07 -0600

    I have the functions written

--- src/hangman/Test.java
+++ src/hangman/Test.java
@@ -1,36 +1,27 @@
-package hangman1;
+package hangman;
 
 import java.util.Scanner;
-
 import edu.princeton.cs.introcs.StdIn;
 import hangman.Utilities;
 
 public class Test {
+	public static int count=0;
 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
-//		String reader = StdIn.readString();
-		String word = Utilities.randomWord();
-		int right = 0;
-		int failure = 0;
+		Utilities.randomWord(); //pick a word
+		
+		
 		Utilities.initSketch(); //draw the hang
-		Utilities.drawBlank(word); //draw the blank
-		while (right <= word.length() && failure <= 7) {	
-			char guess = reader.next().charAt(0);
-//			char guess = reader.charAt(0);
-			// int p = word.indexOf(guess);
-			int p = Utilities.compare(guess, word);
-			if (p < 0) {	
-				failure++;
-				Utilities.hangman(failure);//draw a hangman
-			}
-			else {
-				//draw a letter on screen
-				Utilities.drawLetter(p, String.valueOf(guess));
-				right++;	
-			}
+		//Utilities.drawBlank(word); //draw the blank
+		
+		while (count<7 && Utilities.asterisk.contains("*")) {
+			System.out.println("Guess a letter:");
+			System.out.println(Utilities.asterisk);
+			String guess = reader.next();
+			Utilities.compare(guess);
 		}
-
+		reader.close();
 	}
 
 }
