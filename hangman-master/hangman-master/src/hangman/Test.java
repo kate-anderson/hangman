7c228b676eb8664d6febdff69b88b7dd7561d173
u2huebui <31355459+u2huebui@users.noreply.github.com>
2017-12-06 10:26:38 -0600

    add javadoc html

---
+++ hangman-master/hangman-master/src/hangman/Test.java
@@ -0,0 +1,26 @@
+package hangman;
+
+import java.util.Scanner;
+import edu.princeton.cs.introcs.StdIn;
+import hangman.Utilities;
+
+public class Test {
+	public static int count = 0;
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		Utilities.randomWord(); // pick a word
+
+		Utilities.initSketch(); // draw the hang
+		Utilities.drawBlank(); // draw the blank
+
+		while (count < 7 && Utilities.asterisk.contains("*")) {
+			System.out.println("Guess a letter:");
+			System.out.println(Utilities.asterisk);
+			String guess = reader.next();
+			Utilities.compare(guess);
+		}
+		reader.close();
+	}
+
+}
